c65cd73b3852cc54a63cc18b81670d2e10a542d8
Darnycya <Darnycya@gmail.com>
2021-03-01 15:09:43 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Results from './Components/Results';
+import Keypad from './Components/Keypad';
+import Modal from './Components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: "",
+      show: false,
+    }
+    }
+  
+    showModal = e => {
+      this.setState({
+        show: !this.state.show
+      });
+    };
+
+   
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        } else if (this.state.result.includes('4')) {
+          checkResult = this.showModal();
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+      
+
+        
+  };
+  
+  player = () => {
+    if (this.state.result === 4) {
+    console.log('yes')
+    } else {
+      return this.state.result
+  }
+  }
+
+  
+
+    render() {
+      return (
+        <>
+         
+                <div className="calculator-body">
+                    
+                    <Results result={this.state.result}/>
+                    <Keypad onClick={this.onClick}/>
+          </div>
+          
+          <button
+            
+            class="toggle-button"
+            id="centered-toggle-button"
+            onClick={e => {
+              this.showModal();
+         }}
+          >  <p className="X">X</p> </button>
+
+              
+          <Modal
+            onClose={this.showModal}
+            show={this.state.show}>
+            
+
+          <div className="image">
+              <img src="https://res.cloudinary.com/darnycya/image/upload/v1612424104/Dak_ynbhmz.png"></img></div>
+            
+            
+          </Modal>
+          </>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/Modal.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Modal.css";
+
+
+
+export default class Modal extends React.Component {
+
+  onClose = e => {
+    this.props.onClose && this.props.onClose(e);
+  };
+
+  whichPlayer(num) {
+    if (num = 4) {
+      return 
+    }
+  }
+
+
+
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div class="modal" id="modal">
+        <div class="content">{this.props.children}</div>
+        <div class="actions">
+          <button class="close-button" onClick={this.onClose}>
+            <p className="X">X</p>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
